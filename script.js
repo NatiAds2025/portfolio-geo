744627797c561dab6ce433c13d92fdb3013254cf
Natalia <ntgetto@gmail.com>
2026-06-16 21:16:24 -0300

    inclusão comentários
    inclui os comentários pedido na atividade

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const botaoTema = document.getElementById("tema");
+const botaoTema = document.getElementById("tema"); //Procura no HTML um elemento que tenha o id="tema" (o seu botão) e o guarda em uma variável chamada botaoTema para podermos usá-lo depois.
 
-botaoTema.addEventListener("click", () => {
+botaoTema.addEventListener("click", () => {   //Quando o usuário clicar neste botão, execute todo o código que está aqui dentro".
 
-    document.body.classList.toggle("tema-escuro");
+    document.body.classList.toggle("tema-escuro"); //A função toggle funciona como um interruptor. Se o corpo do site não tiver a classe "tema-escuro", ela coloca. Se já tiver, ela retira
 
-    if(document.body.classList.contains("tema-escuro")){
-        botaoTema.textContent = "Claro";
+    if(document.body.classList.contains("tema-escuro")){   // se o corpo estiver no tem escuro - ele ativa 
+        botaoTema.textContent = "Claro"; // se não estará no claro 
     } else {
-        botaoTema.textContent = "Escuro";
+        botaoTema.textContent = "Escuro"; // se não ficará escuro 
     }
 
 });
 
 
-const formulario = document.getElementById("formContato");
+const formulario = document.getElementById("formContato"); //Procura no HTML o formulário que tem o id="formContato" e o guarda na variável formulario.
 
-formulario.addEventListener("submit", function(event){
+formulario.addEventListener("submit", function(event){  //No momento em que o usuário tentar enviar os dados (clicando no botão de enviar ou apertando Enter), ele ativa essa função e gera um evento
 
-    // Impede o envio padrão do formulário
+    // Impede o envio padrão do formulário, para testar antes de enviar 
     event.preventDefault();
 
     // Captura os valores dos campos
@@ -25,7 +25,7 @@ formulario.addEventListener("submit", function(event){
     const email = document.getElementById("email").value.trim();
     const mensagem = document.getElementById("mensagem").value.trim();
 
-    // Verifica se todos os campos foram preenchidos
+    // Verifica se todos os campos foram preenchidos e nã oestão vazios  e textto tem formato correto 
     if(nome === "" || email === "" || mensagem === ""){
         alert("Por favor, preencha todos os campos.");
         return;
